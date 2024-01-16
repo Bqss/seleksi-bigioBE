4ddbf09e69f7945b6a05281d864fa1692a8570fd
Bqss1 <basofi.cucokmeong12@gmail.com>
2024-01-16 21:54:28 +0700

    [fix] production

--- models/index.js
+++ models/index.js
@@ -7,7 +7,7 @@ const dotenv = require('dotenv');
 
 const process = require('process');
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
+const env = process.env.NODE_ENV || 'production';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
